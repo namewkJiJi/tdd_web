85d36a883855e53d3fe1dde7d5d3390b799bf818
JiJi <1468349372@qq.com>
2025-04-27 19:39:36 +0800

    message 17:change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item,List
 
 class HomePageTest(TestCase):
     # 测试根路由是不是 home.html渲染网页
@@ -12,15 +12,23 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response,"home.html")
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_user = List()
+        list_user.save()
+
         first_item = Item()
         first_item.text = "The first list item"
+        first_item.list = list_user
         first_item.save()
 
         second_item = Item()
         second_item.text = "The second list item"
+        second_item.list = list_user
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list,list_user)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -28,14 +36,16 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,"The first list item"),"The first itemtext is "+first_saved_item.text
+        self.assertEqual(first_saved_item.list,list_user)
         self.assertEqual(second_saved_item.text,"The second list item")
-
+        self.assertEqual(second_saved_item.list,list_user)
 
 
 class ListViewTest(TestCase):
-    def test_diplay_all_itmes(self):
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
+    def test_display_all_itmes(self):
+        list_user = List.objects.create()
+        Item.objects.create(text="item 1",list=list_user)
+        Item.objects.create(text="item 2",list=list_user)
         response = self.client.get("/lists/the-new-page/")
 
         self.assertContains(response,"item 1")
