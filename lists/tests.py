f1ad6352e7f4b6a82a89560f81d5f0cfee656c9b
JiJi <1468349372@qq.com>
2025-04-27 19:08:03 +0800

    message 15:new URL,view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -12,52 +12,54 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response,"home.html")
 
-#     def test_home_page_return_correct_html(self):
-#         request = HttpRequest()
-#         response = home_page(request)
-#         html = response.content.decode("utf-8")
-#         self.assertTrue(html.startswith('<html>'))
-#         self.assertIn("<title>To-Do lists</title>",html)
-#         self.assertTrue(html.endswith("</html>"))
+    def test_can_save_a_post_request(self):
+        response = self.client.post("/",data={"item_text":"A new list item"})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,"A new list item")
+
+    def test_redirects_after_Post(self):
+        response = self.client.post("/",data={"item_text":"A new list item"})
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response["location"],"/lists/the-new-page/")
 
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
-        response = self.client.get("/")
-        self.assertIn("item 1",response.content.decode())
-        self.assertIn("item 2", response.content.decode())
+
+
 
 class ItemModelTest(TestCase):
-    pass
-    # def test_saving_and_retrieving_items(self):
-    #     first_item = Item()
-    #     first_item.text = "The first list item"
-    #     first_item.save()
-    #
-    #     second_item = Item()
-    #     second_item.text = "The second list item"
-    #     second_item.save()
-    #
-    #     saved_items = Item.objects.all()
-    #     self.assertEqual(saved_items.count(),2)
-    #
-    #     first_saved_item = saved_items[0]
-    #     second_saved_item = saved_items[1]
-    #     self.assertEqual(first_saved_item.text,"The first list item"),"The first itemtext is "+first_saved_item.text
-    #     self.assertEqual(second_saved_item.text,"The second list item")
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first list item"
+        first_item.save()
 
-    # def test_can_save_a_post_request(self):
-    #     response = self.client.post("/",data={"item_text":"A new list item"})
-    #
-    #     self.assertEqual(Item.objects.count(),1)
-    #     new_item = Item.objects.first()
-    #     self.assertEqual(new_item.text,"A new list item")
-    #
-    # def test_redirects_after_Post(self):
-    #     response = self.client.post("/",data={"item_text":"A new list item"})
-    #     self.assertEqual(response.status_code,302)
-    #     self.assertEqual(response["location"],"/")
+        second_item = Item()
+        second_item.text = "The second list item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text,"The first list item"),"The first itemtext is "+first_saved_item.text
+        self.assertEqual(second_saved_item.text,"The second list item")
+
+
+
+class ListViewTest(TestCase):
+    def test_diplay_all_itmes(self):
+        Item.objects.create(text="item 1")
+        Item.objects.create(text="item 2")
+        response = self.client.get("/lists/the-new-page/")
+
+        self.assertContains(response,"item 1")
+        self.assertContains(response,"item 2")
+
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-new-page/")
+        self.assertTemplateUsed(response,"list.html")
